000adf9e253f9d3b9c9a9c1ea7fc3435160c21cc
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-11-17 16:47:46 +0530

    api integration for the app

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const btnTranslate = document.querySelector("#btn-translate");
 const txtInput = document.querySelector("#txt-input");
+const output = document.querySelector("#output")
+
+const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+
+
+function getTranslateApp(text){
+    return serverUrl + "?" + "text=" + text
+}
+
+
+function errorHandler(error){
+    console.log(error)
+}
 
 
 function clickHandler(){
-    console.log("clicked");
-    console.log("txtInput", txtInput.value)
+ var inputText = txtInput.value
+ fetch(getTranslateApp(inputText))
+ .then(response => response.json())
+ .then(json => output.innerHTML = json.contents.translated)
+ .catch(errorHandler)
 }
 
 
